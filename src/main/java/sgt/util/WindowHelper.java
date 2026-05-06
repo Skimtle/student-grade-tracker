ec446b12221f74b5e3464f4be9e6ab5d1d165656
Dopamine2006 <jilliancarlmartypascasio@gmail.com>
2026-05-06 19:05:16 +0800

    Add files via upload

---
+++ src/main/java/sgt/util/WindowHelper.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sgt.util;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Cii
+ */
+public class WindowHelper {
+    
+    public static void openWindow(JFrame parent, JFrame newWindow) {
+        newWindow.setSize(parent.getSize());
+        newWindow.setLocationRelativeTo(null);
+        newWindow.setVisible(true);
+        parent.dispose();
+    }
+}
